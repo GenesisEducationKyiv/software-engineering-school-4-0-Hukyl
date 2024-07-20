bbaa2f04c1f6992fdd8e2176fc94e999f3a3fbcc
Andrii Shalaiev <a.shalaev7125@gmail.com>
2024-07-20 19:09:15 +0300

    feature: update config for saga and update message types

--- pkg/broker/messages.go
+++ pkg/broker/messages.go
@@ -4,12 +4,12 @@ type Command struct {
 	ID        string `json:"commandID"`
 	Type      string `json:"commandType"`
 	Timestamp string `json:"timestamp"`
-	Data      string `json:"data"`
+	Data      any    `json:"data"`
 }
 
 type Event struct {
 	ID        string `json:"eventID"`
 	Type      string `json:"eventType"`
 	Timestamp string `json:"timestamp"`
-	Data      string `json:"data"`
+	Data      any    `json:"data"`
 }
